c0133648515eb8af1ab47cda1ba17b6054226b31
Staiks <staiksdono@gmail.com>
2026-02-23 22:41:39 +0000

    Update roles.sql

--- supabase/code/volumes/db/roles.sql
+++ supabase/code/volumes/db/roles.sql
@@ -1,8 +1,10 @@
 -- NOTE: change to your own passwords for production environments
 \set pgpass `echo "$POSTGRES_PASSWORD"`
 
+ALTER USER postgres WITH PASSWORD :'pgpass';
 ALTER USER authenticator WITH PASSWORD :'pgpass';
 ALTER USER pgbouncer WITH PASSWORD :'pgpass';
+ALTER USER supabase_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
